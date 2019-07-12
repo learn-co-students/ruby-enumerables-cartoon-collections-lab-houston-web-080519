f075c22706fb740efac4ae56bf515ce425d399b8
Barrette Banner <barrettebanner@gmail.com>
2019-07-12 16:36:28 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index do |item, index|
+    puts "#{index + 1}. #{item} "
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  new_array = []
+  array.each do |word| 
+    new_array.push("#{word.capitalize}!")
+    
+  end
+  return new_array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  less_than_4 = false  
+  array.select do |word|
+   if word.length > 4
+     less_than_4 = true
+   end
+ end
+   return less_than_4 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |food|
+    cheese_types.include?(food)
+  end
 end
