f196e7d1b0f3ac038811cf3e3abd52565e955f48
Jun Liang <illuminatemynight@gmail.com>
2019-08-05 00:42:21 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,42 +1,31 @@
-def roll_call_dwarves(array)
-  i = 0
-  while i < array.length
-  puts "#{i + 1}. #{array[i]}"
-  i += 1
-end
+def roll_call_dwarves(dwarves)
+
+  dwarves.each_with_index { |item, index|
+  puts "#{index + 1}. #{item}"
+}
+
 end
 
-def summon_captain_planet(array)
-  def summon_captain_planet(array)
-  nu_array = []
-  i = 0
-  while i < array.length
-    nu_array << array[i].capitalize + "!"
-    i += 1
-  end
-  nu_array
-end
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect { |x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls(array)
-  i = 0
-  if  array.any? {|i| i.length > 4}
-    return true
+def long_planeteer_calls(calls)
+  calls.each_with_index { |item, index|
+    if item.length > 4
+      return true
+    end
+  }
+  return false
+end
+
+def find_the_cheese(snacks)
+  if snacks.include?("gouda")
+    return "gouda"
+  else if snacks.include?("cheddar")
+    return "cheddar"
   else
-    return false
-  i = i + 1
+    return nil
   end
 end
-
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |type|
-cheese_types.include?(type)
-end
-
-def find_the_cheese2(array, array2)
-  cheese_types = array2
-    array.find do |type|
-    cheese_types.include?(type)
-  end
 end
